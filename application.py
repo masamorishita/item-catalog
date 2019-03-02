72e858148604fd8d91771dcffa40b9ce8519e559
Masa Morishita <masam@masamorishita.com>
2019-03-02 06:10:33 +0000

    Modify html templates

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Item
 
@@ -12,17 +12,20 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+#Show all categories and items
 @app.route('/')
+@app.route('/categories')
 def item():
-    category = session.query(Category)
-    items = session.query(Item)
+    category = session.query(Category).order_by(asc(Category.name))
+    items = session.query(Item).order_by(asc(Item.id))
     return render_template('item.html', category=category, items=items)
 
+#Show all items in each category
 @app.route('/categories/<int:category_id>/item')
 def itemWithCategory(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
     items = session.query(Item).filter_by(category_id=category_id)
-    return render_template('item.html', category=category, items=items, category_id=category_id)
+    return render_template('item_by_category.html', category=category, items=items, category_id=category_id)
 
 if __name__ == '__main__':
     app.debug = True
