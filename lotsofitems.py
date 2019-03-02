4baab3b79852113ca15aa89353ce029c82c48e06
Masa Morishita <masam@masamorishita.com>
2019-03-02 05:57:29 +0000

    Add more items

--- lotsofitems.py
+++ lotsofitems.py
@@ -33,3 +33,22 @@ item2 = Item(user_id=1, name="Goggle", description="Goggle is very important for
 
 session.add(item2)
 session.commit()
+
+
+#Item for baseball
+category2 = Category(user_id=1, name="Baseball")
+
+session.add(category2)
+session.commit()
+
+item1 = Item(user_id=1, name="Bat", description="A tool to hit a ball.",
+    category=category2)
+
+session.add(item1)
+session.commit()
+
+item2 = Item(user_id=1, name="Glove", description="A tool to catch a ball.",
+    category=category2)
+
+session.add(item2)
+session.commit()
