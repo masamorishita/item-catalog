aa54a8a549feba317e689fd3f6213b00a9cd8f8b
Masa Morishita <masam@masamorishita.com>
2019-04-04 04:25:55 +0000

    Add item information

--- application.py
+++ application.py
@@ -21,11 +21,18 @@ def item():
     return render_template('item.html', category=category, items=items)
 
 #Show all items in each category
-@app.route('/catalog/<category_name>/item/')
+@app.route('/catalog/<category_name>/items/')
 def itemWithCategory(category_name):
     category = session.query(Category).filter_by(name=category_name).one()
     items = session.query(Item).filter_by(category=category)
     return render_template('item_by_category.html', category=category, items=items)
+
+#Show item's description
+@app.route('/catalog/<category_name>/<item_name>/')
+def itemInformation(category_name, item_name):
+    category = session.query(Category).filter_by(name=category_name).one()
+    items = session.query(Item).filter_by(category=category, name=item_name).one()
+    return render_template('item_information.html', category=category, items=items)
 
 #Create a new item
 @app.route('/catalog/<int:category_id>/item/new/', methods=['GET', 'POST'])
